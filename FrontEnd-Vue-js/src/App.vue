616ce0405b43392920c2fb9259540caa9e2c8070
ShubhenduSrivastava <71115938+ShubhenduSrivastava@users.noreply.github.com>
2024-06-02 14:13:51 +0530

    Create App.vue

---
+++ FrontEnd-Vue-js/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+ 
+ <nav class="navbar bg-primary" data-bs-theme="dark">
+    <div class="container-fluid">
+    <a class="navbar-brand" href="#">Beaches To Visit</a>
+    </div>
+ </nav>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
