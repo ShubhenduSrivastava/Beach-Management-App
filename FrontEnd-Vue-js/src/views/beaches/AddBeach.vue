769411c2210c05159539826e597d649e7be3c8a0
ShubhenduSrivastava <71115938+ShubhenduSrivastava@users.noreply.github.com>
2024-06-02 14:10:44 +0530

    Create AddBeach.vue

---
+++ FrontEnd-Vue-js/src/views/beaches/AddBeach.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { reactive } from 'vue';
+import axios from 'axios';
+import { useRouter } from 'vue-router';
+
+
+let newBeach = reactive({
+    beachName:"",
+    place: "",
+    imageUrl:""
+});
+
+const router = useRouter();
+
+const addBeach = () => {
+    axios.post("http://localhost:5083/Beach", newBeach).then(() => {
+        router.push("/");
+    });
+};
+
+</script>
+
+<template>
+    <div class="container mt-5">
+        <form @submit.prevent="addBeach">
+            <legend>Add a New item</legend>
+            <div class="mb-3">
+                <label for="txtBeachName" class="form-label">Beach Name</label>
+                <input type="text" class="form-control" id="txtBeachName" aria-describedby="emailHelp" v-model="newBeach.beachName">
+               
+            </div>
+            <div class="mb-3">
+                <label for="txtLocation" class="form-label">Location</label>
+                <input type="text" class="form-control" id="txtLocation" aria-describedby="emailHelp" v-model="newBeach.place">
+               
+            </div>
+            <div class="mb-3">
+                <label for="txtImageUrl" class="form-label">Image URL</label>
+                <input type="text" class="form-control" id="txtImageUrl" aria-describedby="emailHelp" v-model="newBeach.imageUrl">
+                
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</template>
